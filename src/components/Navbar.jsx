abd3ba64be1dcc6fc4dbd931cbc621de5f2d49fc
Kundan Agrawal <123254080+Kundanagrawalofficial@users.noreply.github.com>
2023-05-15 10:10:46 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
           }}>
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Andrew &nbsp;
+            Kundan &nbsp;
             <span className='sm:block hidden'> | Full Stack Developer</span>
           </p>
         </Link>
